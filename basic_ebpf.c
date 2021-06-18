b18269622ba818d836a5122a89201db6e31a48a2
Pedro Fausto <pedrofausto@gmail.com>
2021-06-18 12:26:00 -0300

    Minor adjustments

--- basic_ebpf.c
+++ basic_ebpf.c
@@ -1,3 +1,4 @@
+#define KBUILD_MODNAME "tc"
 
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
@@ -6,6 +7,9 @@
 #include <linux/if_ether.h>
 #include <linux/ip.h>
 #include <linux/tcp.h>
+#include <linux/sched.h>
+
+
 
 #ifndef __section
 # define __section(NAME)                  \
@@ -13,7 +17,27 @@
 #endif
 #define htons(x) ((__be16)___constant_swab16((x)))
 
-static int drop_pkt(struct __sk_buff *skb)
+// #define TASK_COMM_LEN	16
+
+// static inline int check_process(void)
+// {
+// 	const char name[] = "telnet";
+// 	char task_name[TASK_COMM_LEN] = { 0 };
+// 	const struct task_struct *t;
+
+// 	t = (struct task_struct *) bpf_get_current_task();
+
+// 		bpf_get_current_comm(&task_name, sizeof(task_name));
+
+// 	for (int i = 0; i < sizeof(name); ++i) {
+// 		if (task_name[i] != name[i])
+// 			return 0;
+// 	}
+	
+// 	return 1;
+// }
+
+static inline int drop_pkt(struct __sk_buff *skb)
 {
     int eth_off = 0;
     int iphdr_off = 0;
@@ -39,17 +63,22 @@ static int drop_pkt(struct __sk_buff *skb)
        return XDP_DROP;
     }
 }
-__section("prog")
+__section("classifier")
 int xdp_drop(struct __sk_buff *skb)
 {
-    return drop_pkt(skb);
+    // if (check_process())
+    // {
+         return drop_pkt(skb);
+    // }
+    // else
+    //     return XDP_DROP;        
 }
 
 char __license[] __section("license") = "GPL";
 
-/* Loading 
+/* Loading the eBPF program into kernel space
 sudo tc qdisc add dev ens33 clsact
-sudo tc filter add dev ens33 egress bpf direct-action obj basic_ebpf.o sec prog
+sudo tc filter add dev ens33 egress bpf direct-action obj basic_ebpf.o sec classifier
 tc filter show dev ens33 egress
 
 *** Unloading
